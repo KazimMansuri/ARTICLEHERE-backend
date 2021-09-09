eba0fbb0edb6441c7ad9f7b9f66734298dd11c6a
Kazim <mansurikazim5@gmail.com>
2021-09-09 16:21:54 +0530

    initial commit

---
+++ api/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, HttpResponse
+from .models import Article
+from .serializers import ArticleSerializer, UserSerializer
+from rest_framework import viewsets             # for modelviewset, generic viewset , viewset
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from django.contrib.auth.models import User
+
+
+
+############### model view set #####################
+
+
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer 
+    permission_classes = [IsAuthenticated]
+    authentication_classes = (TokenAuthentication,)
+
+
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
